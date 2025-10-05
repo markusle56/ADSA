781a9b64f771f0a52c2b8875cee00c6c282457fb
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-10-05 13:53:54 +1030

    fix

--- main.py
+++ main.py
@@ -1,35 +1,63 @@
-class HashTable():
+
+NEVER_USED = 0
+TOMBSTONE = 1
+OCCUPIED = 2
+
+class HashTable:
     def __init__(self):
-        alphabet = list("abcdefghijklmnopqrtsuvwxyz")
-        self.words = {key : "" for key in alphabet}
-        self.status = {key: "never used" for key in alphabet}
-        
-    def insert(self, word: str):
-        last = word[-1]
-        if self.status[last] == "occupied":
-            return 
-        else:
-            self.words[last] = word
-            self.status[last] = "occupied"
-        return 
-    def deletion(self, word: str):
-        last = word[-1]
-        if self.words[last] == word:
-            self.status[last] = "tombstone"
-    def search(self, word: str):
-        for key, value in self.words.items():
-            if value == word:
-                if self.status[key] == "occupied":
-                    return True
-        return False
-            
-    def print(self):
-        data = []
-        for key, status in self.status.items():
-            if status == "occupied":
-                data.append(self.words[key])
-        print(" ".join(data))
-        return 
+        self.table = [(NEVER_USED, "") for _ in range(26)]
+
+    def _idx(self, ch: str) -> int:
+        return ord(ch) - ord('a')
+
+    def _find_slot(self, key: str):
+        start = self._idx(key[-1])
+        i = start
+        while True:
+            status, k = self.table[i]
+            if status == NEVER_USED:
+                return (False, i)
+            if status == OCCUPIED and k == key:
+                return (True, i)
+            i = (i + 1) % 26
+            if i == start:
+                return (False, start) 
+
+    def search(self, key: str) -> bool:
+        found, _ = self._find_slot(key)
+        return found
+
+    def insert(self, key: str):
+        if self.search(key):
+            return
+        start = self._idx(key[-1])
+        i = start
+        while True:
+            status, _ = self.table[i]
+            if status != OCCUPIED:   
+                self.table[i] = (OCCUPIED, key)
+                return
+            i = (i + 1) % 26
+            if i == start:
+                return
+       
+    def delete(self, key: str):
+        start = self._idx(key[-1])
+        i = start
+        while True:
+            status, k = self.table[i]
+            if status == NEVER_USED:
+                return
+            if status == OCCUPIED and k == key:
+                self.table[i] = (TOMBSTONE, "")
+                return
+            i = (i + 1) % 26
+            if i == start:
+                return
+
+    def output(self) -> str:
+        return " ".join(k for (status, k) in self.table if status == OCCUPIED)
+
 def main():
     prompt = input()
     prompt = prompt.split()
@@ -38,8 +66,8 @@ def main():
         if action[0] == "A":
             hashtable.insert(action[1:])
         elif action[0] == "D":
-            hashtable.deletion(action[1:])
-    hashtable.print()
+            hashtable.delete(action[1:])
+    print(hashtable.output())
     return 
 
 if __name__ == "__main__":
